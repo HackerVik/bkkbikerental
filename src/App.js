e5d8ce6c6eb9f0e1fef8d454c4efd7c03c526731
BihackerViktor <bihacker.viktor@gmail.com>
2019-12-17 18:19:25 +0100

    custom ico

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import biker from './biker.gif';
 import Map from 'pigeon-maps'
-import Marker from 'pigeon-marker'
 import Overlay from 'pigeon-overlay'
+
 
 export default class App extends Component {
 
@@ -47,48 +47,64 @@ export default class App extends Component {
         const {isLoaded, items} = this.state;
         if (!isLoaded) {
             return <div>
-                <img alt={"loadingpicture"} src={biker}
+                <img alt={"loading"} src={biker}
                      style={{
                          borderRadius: "50%",
-                         boxShadow: ".5em .5em .5em grey"
+                         boxShadow: ".5em .5em .5em grey",
+                         border: "1px dotted double grey",
                      }}/></div>;
         } else {
             return (
                 <div>
-                    <Map center={[47.50, 19.06]} zoom={13} width={800} height={800}>
+                    <Map center={[47.505, 19.06]}
+                         zoom={13}
+                         width={800}
+                         height={800}
+                         maxZoom={15}
+                    >
                         {items.map(item => (
-                            <Marker anchor={[item.lat, item.lon]} payload={1} onClick={({event, anchor, payload}) => {
-                            }}/>
+                            <Overlay key={item.id}
+                                     anchor={[item.lat, item.lon]}
+                                     payload={1}
+                                     onClick={({event, anchor, payload}) => {
+                                     }}>
+                                <img style={{
+                                    borderRadius: "50%",
+                                    boxShadow: ".2em .2em .2em grey",
+                                    border: "1px dotted double grey"
+                                }} src={biker}
+                                     width={25} height={25} alt={item.id}/>
+                            </Overlay>
                         ))}
                     </Map>
-                    {/*<table style={{border: "1px solid black"}}>*/}
-                    {/*    <thead>*/}
-                    {/*    <tr style={{border: "1px solid black"}}>*/}
-                    {/*        <th style={{border: "1px solid black"}}>code</th>*/}
-                    {/*        <th style={{border: "1px solid black"}}>name</th>*/}
-                    {/*        <th style={{border: "1px solid black"}}>bikes</th>*/}
-                    {/*        <th style={{border: "1px solid black"}}>spaces</th>*/}
-                    {/*        <th style={{border: "1px solid black"}}>id</th>*/}
-                    {/*        <th style={{border: "1px solid black"}}>lat</th>*/}
-                    {/*        <th style={{border: "1px solid black"}}>lon</th>*/}
-                    {/*        <th style={{border: "1px solid black"}}>type</th>*/}
-                    {/*    </tr>*/}
-                    {/*    </thead>*/}
-                    {/*    <tbody>*/}
-                    {/*    {items.map(item => (*/}
-                    {/*        <tr key={item.id}>*/}
-                    {/*            <td style={{border: "1px solid black", textAlign: "center"}}>{item.code}</td>*/}
-                    {/*            <td style={{border: "1px solid black", textAlign: "center"}}>{item.name}</td>*/}
-                    {/*            <td style={{border: "1px solid black", textAlign: "center"}}>{item.bikes}</td>*/}
-                    {/*            <td style={{border: "1px solid black", textAlign: "center"}}>{item.spaces}</td>*/}
-                    {/*            <td style={{border: "1px solid black", textAlign: "center"}}>{item.id}</td>*/}
-                    {/*            <td style={{border: "1px solid black", textAlign: "center"}}>{item.lat}</td>*/}
-                    {/*            <td style={{border: "1px solid black", textAlign: "center"}}>{item.lon}</td>*/}
-                    {/*            <td style={{border: "1px solid black", textAlign: "center"}}>{item.type}</td>*/}
-                    {/*        </tr>*/}
-                    {/*    ))}*/}
-                    {/*    </tbody>*/}
-                    {/*</table>*/}
+                    <table style={{border: "1px solid black"}}>
+                        <thead>
+                        <tr style={{border: "1px solid black"}}>
+                            <th style={{border: "1px solid black"}}>code</th>
+                            <th style={{border: "1px solid black"}}>name</th>
+                            <th style={{border: "1px solid black"}}>bikes</th>
+                            <th style={{border: "1px solid black"}}>spaces</th>
+                            <th style={{border: "1px solid black"}}>id</th>
+                            <th style={{border: "1px solid black"}}>lat</th>
+                            <th style={{border: "1px solid black"}}>lon</th>
+                            <th style={{border: "1px solid black"}}>type</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {items.map(item => (
+                            <tr key={item.id}>
+                                <td style={{border: "1px solid black", textAlign: "center"}}>{item.code}</td>
+                                <td style={{border: "1px solid black", textAlign: "center"}}>{item.name}</td>
+                                <td style={{border: "1px solid black", textAlign: "center"}}>{item.bikes}</td>
+                                <td style={{border: "1px solid black", textAlign: "center"}}>{item.spaces}</td>
+                                <td style={{border: "1px solid black", textAlign: "center"}}>{item.id}</td>
+                                <td style={{border: "1px solid black", textAlign: "center"}}>{item.lat}</td>
+                                <td style={{border: "1px solid black", textAlign: "center"}}>{item.lon}</td>
+                                <td style={{border: "1px solid black", textAlign: "center"}}>{item.type}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
                 </div>
             );
         }
